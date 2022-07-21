731ff0691c71d0fdca361d387aeed82566ec3388
Avani <avanitiwari2002@gmail.com>
2022-07-21 23:36:14 +0530

    push

--- src/App.js
+++ src/App.js
@@ -1,24 +1,103 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
+import Navbar from "./Navbar";
+
+import { useState } from "react";
+
+import {useRef} from "react";
 
 function App() {
+ 
+  const h= window.innerHeight -80;
+  const myRef={useRef}
+    const goToTop = () => {
+      console.log("clicked");
+      window.scrollTo({top: window.innerHeight -80, left: 0, behavior: 'smooth'});
+      // myRef.current.scrollIntoView() ;
+  };
+
+ 
+
   return (
+   <> 
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+        <section id="start"><React.Fragment><Navbar /></React.Fragment>
+        <div className="scroll"  onClick={goToTop}></div>
+             <div className="scroll"  onClick={goToTop} style={{  bottom: "100px"}}></div>
+  
+            </section> 
+        <section id="About" ref={myRef} >
+      
+          <div className="wrapper">
+          
+        
+         <p className="about_content">
+         <h1  style={{"font-size": "60px"}}><b />
+       <div style={{color:"purple", display:"inline"}} >PERCEPTION </div> and <div style={{color:"orangered", display:"inline"}} >ACTIVE COGNITION</div > LAB
+          </h1>
+          The main question driving work in the lab is how the brain constructs our subjective experience
+            of the world as we actively engage with our surroundings. We study the interaction of
+perception, attention, memory and action within a cognitive neuroscience framework.
+Specifically, our approach can be described as <p style={{color:"orangered", display:"inline"}} >active cognition</p >: while most studies in the field
+are based on responses to an abstract stimulus on a single trial during steady fixation, our work
+examines the way that solving cognitive tasks involves attention and perception interacting
+with context, action (particularly eye movements), memory, emotion and the tasks goals. This
+research includes a consideration of individual differences, including lifespan changes and
+clinical differences in cognitive function.
+</p>
+   <p className='about_content'>
+       
+        Research in the lab currently focusses on three main themes:
+      <li>1. Temporal aspects of cognition</li>
+      <li>2. Prediction and sensorimotor integration</li>
+      <li>3. Individual, clinical and mood-based differences in speed of information processing</li>
+
+
+
+     
+</p>
+<p className='about_content'>
+        The Perception and Active Cognition Lab is part of the Division of Science at New York
+University Abu Dhabi. We are hosted in the Computational Research Building (A2), which
+includes state-of-the-art research facilities, including a research-only MRI scanner, MEG and
+EEG, as well as behavioural, eye-tracking and motion-tracking laboratories. We make use of co-
+registered eyetracking with EEG, MEG and MRI in order to study brain responses under more
+naturalistic viewing conditions in which participants are able to move their eyes during the task.
+
+       </p>
+
+          </div>
+
+        </section>
+
+        <section id="Research">
+         
+          <div className="research_content">
+          <h1>
+            RESEARCH AND PUBLICATION
+          </h1>
+          The main question driving work in the lab is how the brain constructs our subjective experience
+of the world as we actively engage with our surroundings. We study the interaction of
+perception, attention, memory and action within a cognitive neuroscience framework.
+Specifically, our approach can be described as &quot;active cognition&quot;: while most studies in the field
+are based on responses to an abstract stimulus on a single trial during steady fixation, our work
+examines the way that solving cognitive tasks involves attention and perception interacting
+with context, action (particularly eye movements), memory, emotion and the tasks goals. This
+research includes a consideration of individual differences, including lifespan changes and
+clinical differences in cognitive function.
+          </div>
+
+
+        </section>
+
+
+    
     </div>
+    </>
   );
 }
 
---
+++ src/Navbar.js
@@ -0,0 +1,123 @@
+import { useState } from "react";
+import { useEffect } from "react";
+import Logo from "./logo.png";
+import "./Navbar.css"
+import React from "react";
+
+
+
+
+function Navbar(){
+
+    const t = React.createRef()
+
+     
+    
+
+    let listener = null
+    const [scrollState, setScrollState] = useState("top");
+    useEffect(() => {
+        listener = document.addEventListener("scroll", e => {
+          var scrolled = document.scrollingElement.scrollTop
+          if (scrolled >= 40 || window.pageYOffset !== 0) {
+            console.log(window. pageYOffset)
+
+            if (scrollState !== "amir") setScrollState("amir")
+          } else {
+            if (scrollState !== "top") setScrollState("top")
+          }
+        })
+        return () => {
+          document.removeEventListener("scroll", listener)
+        }
+      }, [scrollState])
+    
+
+    const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
+
+
+    useEffect(() => {
+    window.addEventListener("resize", () => {
+        const ismobile = window.innerWidth < 768;
+        if (ismobile !== isMobile) setIsMobile(ismobile);
+    }, false);
+    }, [isMobile]);
+
+
+    const [isActive, setActive] = useState(false);
+    
+    
+    function check(isMobile, isActive) {
+        if(!isMobile) 
+        {
+            
+     
+                return "main_list";
+        
+        }
+
+        else{
+            if(isActive)
+            {
+                return "main_list";
+            }
+
+            else{
+                return "show_list";
+            }
+        }
+        return "main_list";
+    }
+
+
+    function check2(scrollState)
+    {
+        if(scrollState==='top')
+        {return "container";}
+        else
+        {
+            return ["scrolled", "visibile"];
+        }
+    }
+
+    const scr=check2(scrollState)
+    const name= check(isMobile, isActive);
+
+    console.log(name)
+    const toggleClass = () => {
+        console.log("clicked");
+        setActive(!isActive);
+        console.log(isActive);
+      };
+    return(
+
+        <nav className="nav" id={scr[0]}>
+        <div className="container">
+            <div className="logo" >
+                <a href="#"></a>
+               <img src={Logo} alt="logo of the lab" id="logo"></img>
+            </div>
+            <div id="mainListDiv" className={name} >
+                <ul className="navlinks">
+                    <li><a href="#About">About</a></li>
+                    <li><a href="#Research">Research</a></li>
+                    <li><a href="#People">People</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </div>
+            <span className="navTrigger" onClick={toggleClass}>
+                <i></i>
+                <i></i>
+                <i></i>
+            </span>
+        </div>
+    </nav>
+
+
+
+
+    );
+}
+
+
+export default Navbar;
