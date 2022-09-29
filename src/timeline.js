13d334ebfa63abc59384e0c5d96f6bd156c14c8a
Avani <avanitiwari2002@gmail.com>
2022-09-29 14:09:23 -0400

    update added a few papers

--- src/timeline.js
+++ src/timeline.js
@@ -15,11 +15,16 @@ function Timeline()
     iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
     >
 
-    <h3 className="vertical-timeline-element-title">2022-present</h3>
+    <h3 className="vertical-timeline-element-title">Temporal aspects of cognition and neural processing</h3>
     <li>
-        <ul> Paper 1</ul>
-        <ul> Paper 2</ul>
-        <ul> Paper 3</ul>
+        <ul> Acunzo D, Melcher D. Changes in alpha and theta band activity at the time of saccades:
+separate contributions of visual and non-visual signals <a href='https://doi.org/10.31234/osf.io/c29xm' >link</a></ul>
+        <ul>Ronconi L, Balestrieri E, Baldauf D, Melcher D. Distinct cortical networks subserve
+spatio-temporal sampling in vision through different oscillatory rhythms.<a href="https://doi.org/10.1101/2022.05.09.491140">link</a>
+         </ul>
+
+        <ul>Liu X, Balestrieri E, Melcher D. (2022) Evidence for a theta-band behavioral oscillation
+in rapid face detection, European Journal of Neuroscience <a href='https://onlinelibrary.wiley.com/doi/10.1111/ejn.15790'>link</a></ul>
        
     </li>
    
